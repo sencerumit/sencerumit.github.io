655f03ce1c70cd09c818d4e135634bcdb318a73f
Ümit Sencer <sencerumit@gmail.com>
2025-05-15 16:36:44 +0300

    Updates

--- src/components/LoadingSpinner.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const LoadingSpinner = () => (
-  <div className="animate-spin rounded-full h-6 w-6 border-t-2 border-matrix"></div>
-);
-
-export default LoadingSpinner; 
